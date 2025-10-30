2a6c36d6d43befae63eb3be2b09e4c8f2b26e1ad
nirmala0920 <snirmala0920@gmail.com>
2025-10-30 07:55:09 +0530

    Update Nirmala.java

--- Nirmala.java
+++ Nirmala.java
@@ -3,6 +3,7 @@ public class Nirmala
     public static void main(String[] args)
     {
       System.out.println("Hello world");
+       System.out.println("global information tracker");
     }
 
   }
